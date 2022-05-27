b875a9c2f50e97e55369285bff4d9aa0885a60fe
Chathura <chathura.madura11@gmail.com>
2022-05-28 01:40:21 +0530

    component to show my services

---
+++ src/components/Services/Services.jsx
@@ -0,0 +1,89 @@
+import React, { useContext } from "react";
+import "./Services.css";
+import Card from "../Card/Card";
+import Design from "../../img/design.png";
+import Glasses from "../../img/programmer.png";
+import Cam from "../../img/camera.png";
+import { themeContext } from "../../Context";
+import { motion } from "framer-motion";
+import Resume from './resume.pdf';
+
+const Services = () => {
+  // context
+  const theme = useContext(themeContext);
+  const darkMode = theme.state.darkMode;
+
+  // transition
+  const transition = {
+    duration: 1,
+    type: "spring",
+  };
+
+  return (
+    <div className="services" id="services">
+      {/* left side */}
+      <div className="awesome">
+        {/* dark mode */}
+        <span style={{ color: darkMode ? "white" : "" }}>My </span>
+        <span>services</span>
+        <spane>
+          Lorem ispum is simpley dummy text of printing of printing Lorem
+          <br />
+          ispum is simpley dummy text of printing
+        </spane>
+        <a href={Resume} download>
+          <button className="button s-button">Download CV</button>
+        </a>
+        <div className="blur s-blur1" style={{ background: "#ABF1FF94" }}></div>
+      </div>
+      {/* right */}
+      <div className="cards">
+        {/* first card */}
+        <motion.div
+          initial={{ left: "25rem" }}
+          whileInView={{ left: "14rem" }}
+          transition={transition}
+        >
+          <Card
+            emoji={Design}
+            heading={"Design"}
+            detail={"Figma, Photoshop, Adobe Illustrator"}
+          />
+        </motion.div>
+        {/* second card */}
+        <motion.div
+          initial={{ left: "-11rem", top: "12rem" }}
+          whileInView={{ left: "-4rem" }}
+          transition={transition}
+        >
+          <Card
+            emoji={Glasses}
+            heading={"Developer"}
+            detail={"Html, Css, JavaScript, Java, React, Nodejs, React-native, Angular, C++, Scala"}
+          />
+        </motion.div>
+        {/* 3rd */}
+        <motion.div
+          initial={{ top: "19rem", left: "25rem" }}
+          whileInView={{ left: "12rem" }}
+          transition={transition}
+        >
+          <Card
+            emoji={Cam}
+            heading={"Photography and Videography"}
+            detail={
+              "When words become unclear, I shall focus with photographs. B Day Parties, Functions"
+            }
+            color="rgba(252, 166, 31, 0.45)"
+          />
+        </motion.div>
+        <div
+          className="blur s-blur2"
+          style={{ background: "#ABF1FF94" }}
+        ></div>
+      </div>
+    </div>
+  );
+};
+
+export default Services;
