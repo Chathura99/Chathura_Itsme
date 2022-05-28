3d41a8f14ede49314b5447e3695c4b69274218f8
Chathura <chathura.madura11@gmail.com>
2022-05-28 22:14:49 +0530

    make mobile responsive

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -27,34 +27,42 @@ const Portfolio = () => {
           <img src={""} alt="" />
           <p><a href="#">The-branded-shop</a> <br/>E-commerce Web Application</p>
         </SwiperSlide>
+
         <SwiperSlide>
           <img src={""} alt="" />
           <p> <a href="https://sasnaka.org/#/" target="_blank" >Sasnaka Volunteer Platform</a><br/> React web application </p>
         </SwiperSlide>
+
         <SwiperSlide>
           <img src={Cal} alt="" />
           <p><a href="#">Simple Calculator</a> <br/>React-native mobile app </p>
         </SwiperSlide>
+
         <SwiperSlide>
           <img src={Todolist} alt="" />
           <p><a href="https://plan-your-date.herokuapp.com/" target="_blank" >Plan-Your-Date</a> <br/> Web Application</p>
         </SwiperSlide>
+
         <SwiperSlide>
           <img src={""} alt="" />
           <p><a href="#" target="_blank" >Employee Data  <br/>Management System</a> <br/> Web Application</p>
         </SwiperSlide>
+
         <SwiperSlide>
           <img src={""} alt="" />
           <p><a href="https://tic-tac-toe-play.herokuapp.com/" target="_blank" >Play-TicTacToe</a> <br/> Game </p>
         </SwiperSlide>
+
         <SwiperSlide>
           <img src={""} alt="" />
           <p><a href="#" target="_blank" >Apartment Resource Management System</a> <br/> Web Application</p>
         </SwiperSlide>
+
         <SwiperSlide>
           <img src={Chathura} alt="" />
           <p><a href="https://chathura-itsme.herokuapp.com/" target="_blank" >Myportfolio - Chathura</a> <br/> Web Application</p>
         </SwiperSlide>
+
       </Swiper>
     </div>
   );
